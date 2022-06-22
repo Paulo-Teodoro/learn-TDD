9baf7ad752a9539e56728c9746443573b73374b2
Paulo Teodoro <paulolimrs@outlook.com>
2022-06-21 23:16:48 -0300

    feat: Create Product Class

---
+++ src/Core/Orders/Product.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Core\Orders;
+
+class Product
+{
+    public function __construct(
+        protected string $id,
+        protected string $name,
+        protected float $price,
+        protected int $total
+    ) {}
+
+    public function getId() :string
+    {
+        return $this->id;
+    }
+
+    public function getName() :string
+    {
+        return $this->name;
+    }
+
+    public function getPrice() :string
+    {
+        return $this->price;
+    }
+
+    public function total() :float
+    {
+        return $this->price * $this->total;
+    }
+
+    public function totalWithTax(float $percent) :float
+    {
+        $total = $this->total();
+        return $total + ($percent / 100 * $total);
+    }
+}
